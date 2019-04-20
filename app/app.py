8bb16ff936435daee82842cdfa59e1222bc45a40
Rohit Moni <rohitmdxb@gmail.com>
2019-04-20 19:44:35 -0400

    Second commit. Styles work now. Ignoring pycache.

--- app/app.py
+++ app/app.py
@@ -3,9 +3,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
-url_for('static', filename='app.css')
-
 @app.route('/')
-@app.route('/hello/<name>')
+@app.route('/hello/<string:name>')
 def hello(name=None):
     return render_template('app.html', name=name)
+
+if __name__ == "__main__":
+    app.run()
