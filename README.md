0f0f93f61755853b8d3ba0533895e286a0b27417
Rohit Moni <rohitmdxb@gmail.com>
2019-04-20 23:16:43 -0400

    setting up setup.py file

---
+++ README.md
@@ -0,0 +1,8 @@
+# Basic Python Flask Scaffold
+
+To install:
+
+(Optional)
+Set up a virtual environment and activate it
+
+Run pip install .[dev]
--- setup.py
+++ setup.py
@@ -1,3 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="basic-flask-app-scaffold",
+    version="1.0.0",
+    author="Rohit Moni",
+    author_email="rohitmdxb@gmail.com",
+    description="A scaffold so I can start new flask projects easily",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "Flask"
+    ],
+    extras_require={
+        'dev': [
+            "pylint"
+        ]
+    }
+)
 
 requires: (For later)
 Flask
