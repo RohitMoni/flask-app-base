2d94f93b333006829085b83d41afffac7f20b9d1
Rohit Moni <rohitmdxb@gmail.com>
2019-04-20 22:38:42 -0400

    Example of serving static files

--- app/app.py
+++ app/app.py
@@ -3,7 +3,11 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/hello/<string:name>')
+@app.route("/")
+@app.route("/hello/<string:name>")
 def hello(name=None):
-    return render_template('app.html', name=name)
+    return render_template("app.html", name=name)
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
